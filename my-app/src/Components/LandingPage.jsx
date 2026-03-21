f36ff40af4f145bd493b600a1632d667cdea1a7d
Dr-Venom29 <redhulk2029@gmail.com>
2026-03-22 01:19:28 +0530

    feat: add landing page with intro animation and fix reveal timing using requestAnimationFrame

--- my-app/src/Components/LandingPage.jsx
+++ my-app/src/Components/LandingPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { gsap } from "gsap";
-import "./Landingpage.css";
+import "./LandingPage.css";
 
 class TextScramble {
   constructor(el) {
